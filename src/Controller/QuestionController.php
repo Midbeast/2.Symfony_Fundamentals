1c54db7b6b65f39dcd6ee6bf449525aefe63c0bb
Igor Macevic <64796141+Midbeast@users.noreply.github.com>
2021-10-05 06:08:39 +0200

    Installed the Markdown Bundle and implemented it

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,7 +28,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $parser): Response
     {
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
@@ -35,9 +36,12 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
 
+        $questionText= "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+        $html = $parser->transformMarkdown($questionText);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
+            'questionText' => $html,
         ]);
     }
 }
